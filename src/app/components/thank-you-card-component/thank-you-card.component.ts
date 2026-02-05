335fdab5fdb92f111f563e37cbd62fdf3d844377
raduharangus <radu.harangus@cec.valantic.com>
2026-02-05 14:26:00 +0200

    Remove unused `imports` in ThankYouCardComponent and cleanup global styles

--- src/app/components/thank-you-card-component/thank-you-card.component.ts
+++ src/app/components/thank-you-card-component/thank-you-card.component.ts
@@ -2,7 +2,6 @@ import {Component, input} from '@angular/core';
 
 @Component({
   selector: 'app-thank-you-card',
-  imports: [],
   templateUrl: './thank-you-card.component.html',
   styleUrl: './thank-you-card.component.scss',
 })
